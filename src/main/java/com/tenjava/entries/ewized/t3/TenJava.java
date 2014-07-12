b14d2e6ba9ea55ebaedf17b7a5a6fc7445f7c4e1
Joshua Rodriguez <ewized@gmail.com>
2014-07-12 07:58:29 -0700

    Common Utils

--- src/main/java/com/tenjava/entries/ewized/t3/TenJava.java
+++ src/main/java/com/tenjava/entries/ewized/t3/TenJava.java
@@ -2,4 +2,28 @@ package com.tenjava.entries.ewized.t3;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class TenJava extends JavaPlugin {
+    private static TenJava plugin;
+
+    @Override
+    public void onLoad() {
+        plugin = this;
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    /**
+     * Get this plugin's instance.
+     * @throws java.lang.IllegalArgumentException
+     */
+    public static TenJava get() {
+        checkNotNull(plugin);
+
+        return plugin;
+    }
+}
---
+++ src/main/java/com/tenjava/entries/ewized/t3/util/Common.java
@@ -0,0 +1,37 @@
+package com.tenjava.entries.ewized.t3.util;
+
+import com.tenjava.entries.ewized.t3.TenJava;
+import org.bukkit.ChatColor;
+
+import java.util.logging.Level;
+
+public final class Common {
+    private static boolean debug;
+
+    private Common() {
+        debug = Boolean.parseBoolean(System.getProperty("debug"));
+    }
+
+    /** Debug messages if JVM has debug flag */
+    public static void debug(String message, Object... args) {
+        if (debug) {
+            TenJava.get().getLogger().log(Level.INFO, message, args);
+        }
+    }
+
+    /** Debug stack trace messages */
+    public static void debug(Exception e, boolean simple) {
+        debug(e.getMessage());
+
+        if (!simple) {
+            for (StackTraceElement element : e.getStackTrace()) {
+                debug(element.toString());
+            }
+        }
+    }
+
+    /** Color a string */
+    public static String color(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+}
